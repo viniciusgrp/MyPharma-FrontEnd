73a387527675bb1f4390d10f4b0437d1404bfd8e
Vinicius <viniciusgrp@gmail.com>
2023-04-03 19:01:08 -0300

    fix search to include method

--- src/context/productsContext.jsx
+++ src/context/productsContext.jsx
@@ -7,29 +7,29 @@ export const JobProvider = ({ children }) => {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(undefined);
-  const [cartProducts, setCartProducts] = useState([])
-  const [page, setPage] = useState(0)
-  const [itemsPerPage, setItemsPerPage] = useState(10)
-  const [showCart, setShowCart] = useState(false)
-  const [sort, setSort] = useState("")
-  const [nameSearch, setNameSearch] = useState("")
-  const [productId, setProductId] = useState("")
+  const [cartProducts, setCartProducts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [showCart, setShowCart] = useState(false);
+  const [sort, setSort] = useState("");
+  const [nameSearch, setNameSearch] = useState("");
+  const [productId, setProductId] = useState("");
 
   useEffect(() => {
-    const cart = localStorage.getItem("@CART")
+    const cart = localStorage.getItem("@CART");
     if (cart) {
-      setCartProducts(JSON.parse(cart))
+      setCartProducts(JSON.parse(cart));
     }
-  }, [])
-  
+  }, []);
+
   const resetFilters = () => {
-    setSelectedCategory("")
-    setPage(0)
-    setItemsPerPage(10)
-    setShowCart(false)
-    setSort("")
-    setNameSearch("")
-  }
+    setSelectedCategory("");
+    setPage(0);
+    setItemsPerPage(10);
+    setShowCart(false);
+    setSort("");
+    setNameSearch("");
+  };
 
   const getCategories = async () => {
     try {
@@ -42,16 +42,16 @@ export const JobProvider = ({ children }) => {
   const getProducts = async () => {
     try {
       const category = selectedCategory;
-      const limit = Number(itemsPerPage)
-      const skip = +page * +limit
-      const name = nameSearch
+      const limit = Number(itemsPerPage);
+      const skip = +page * +limit;
+      const name = nameSearch;
       const { data } = await api.get(`/products`, {
         params: {
           category,
           limit,
-          skip, 
-          sort, 
-          name
+          skip,
+          sort,
+          name: `/${name}/i`,
         },
       });
       setProducts(data);
@@ -65,12 +65,32 @@ export const JobProvider = ({ children }) => {
   }, []);
 
   useEffect(() => {
-    getProducts(); 
+    getProducts();
   }, [selectedCategory, itemsPerPage, page, sort, nameSearch]);
 
   return (
     <ProductsContext.Provider
-      value={{ categories, setSelectedCategory, products, cartProducts, setCartProducts, selectedCategory, page, setPage, itemsPerPage, setItemsPerPage,showCart, setShowCart, sort, setSort, nameSearch, setNameSearch, productId, setProductId, resetFilters }}
+      value={{
+        categories,
+        setSelectedCategory,
+        products,
+        cartProducts,
+        setCartProducts,
+        selectedCategory,
+        page,
+        setPage,
+        itemsPerPage,
+        setItemsPerPage,
+        showCart,
+        setShowCart,
+        sort,
+        setSort,
+        nameSearch,
+        setNameSearch,
+        productId,
+        setProductId,
+        resetFilters,
+      }}
     >
       {children}
     </ProductsContext.Provider>
